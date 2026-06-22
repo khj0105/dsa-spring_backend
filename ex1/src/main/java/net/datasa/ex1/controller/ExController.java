fb77e884b673db411595d7ba1507fcf4ca174f0e
khj0105 <khj0001052@gmail.com>
2026-06-22 15:04:34 +0900

    Practice

--- ex1/src/main/java/net/datasa/ex1/controller/ExController.java
+++ ex1/src/main/java/net/datasa/ex1/controller/ExController.java
@@ -1,10 +1,41 @@
 package net.datasa.ex1.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 public class ExController {
 
 	// code here
+	@GetMapping("/introduce")
+	public String introduce() {
+		return "/ex/introduce";
+	}
 	
+	@GetMapping("/notice")
+	public String notice() {
+		return "ex/notice";
+	}
+	
+	@GetMapping("/inquiry")
+	public String inqure() {
+		return  "/ex/inquiry";
+	}
+	
+	@PostMapping("/sendData")
+	public String inquiry(
+			@RequestParam(name = "name") String name
+			, @RequestParam(name = "email") String email
+			, @RequestParam(name = "message") String message
+	) {
+		
+		log.debug("> 문의 - 이름: {}, 이메일: {}, 문의 내용: {}", name, email, message);
+		
+		
+		return "redirect:/";
+	}
 }
--- ex1/src/main/resources/static/js/inquiry.js
+++ ex1/src/main/resources/static/js/inquiry.js
@@ -2,3 +2,30 @@
 
 /* 유효성 검사 작성 */
 /* 이름은 3자 이상 10자 이하, 이메일은 '@'를 포함, 메시지는 5자 이상 */
+
+function formCheck() {
+    let name = document.querySelector("#name").value.trim();
+    let email = document.querySelector('#email').value.trim();
+    let message = document.querySelector('#message').value.trim();
+
+    // 이름 3 - 10자
+    if (name.length < 3 || name.length > 10) {
+        alter("이름은 3자 이상 10자 이하로 작성해주세요.");
+        return false;
+    }
+
+    // 이메일 : @포함
+    if (!email.includes("@")) {
+        alert("올바른 이메일 형식을 입력해주세요.");
+        return false;
+    }
+
+    // 메시지 : 5자 이상
+    if (message.length < 5) {
+        alert("문의 내용은 5자 이상 입력해주세요.");
+        return false;
+    }
+
+    alert("문의가 등록되었씁니다.");
+    return true;
+}
