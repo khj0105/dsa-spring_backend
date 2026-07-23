d2ed4dbf4407c5fd422dffbdbb9ee3c4407f4f5c
khj0105 <khj0001052@gmail.com>
2026-07-23 23:30:50 +0900

    day23

--- web5/src/main/java/net/datasa/web5/service/BoardService.java
+++ web5/src/main/java/net/datasa/web5/service/BoardService.java
@@ -12,11 +12,14 @@ import net.datasa.web5.repository.BoardRepository;
 import net.datasa.web5.repository.MemberRepository;
 import net.datasa.web5.repository.ReplyRepository;
 import net.datasa.web5.util.FileManager;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 	게시판 관련 서비스
@@ -67,5 +70,80 @@ public class BoardService {
 				.build();
 		log.debug("저장되는 게시글 정보: {}", entity);
 		br.save(entity);
+	}
+	
+	// -----------------------------------------------------------------------------------
+	/*
+		게시글 전체 조회
+		@return 글 목록
+	 */
+	public List<BoardDTO> getBoardList() {
+		/*MemberEntity entity = mr.findById()
+				.orElseThrow(() -> new EntityNotFoundException("id에 맞는 "))*/
+		// 1.
+		Sort sort = Sort.by(Sort.Direction.DESC, "boardNum");
+		
+		// 2. 조회 결과 List로 받아오기
+		List<BoardEntity> entityList = br.findAll(sort);
+		
+		// 3. DTO List로 변환
+		List<BoardDTO> dtoList = new ArrayList<>();
+		for (BoardEntity entity : entityList) {
+			BoardDTO dto = BoardDTO.convertToBoardDTO(entity);
+			dtoList.add(dto);
+		}
+		return dtoList;
+	}
+	
+	// -----------------------------------------------------------------------------------
+	/*
+		검색 후 지정한 한 페이지 분량의 글 목록 조회
+		@param page				현재 페이지
+		@param pageSize			한 페이지당 글 수
+		@param searchType		검색 대상 (title, contents, id, all)
+		@param searchWord		검색어
+		@return					한 페이지의 글 목록
+	 */
+	public Page<BoardDTO> searchBoardList(int page, int pageSize, String searchType, String searchWord) {
+		// Page 객체는 번호가 0부터 시작
+		page--;
+		
+		/*
+			Pageable
+				- 몇 번째 페이지를, 몇 개씩, 어떤 정렬로 가져올 것인지 정의한 인터페이스
+				- PageRequest.of(페이지 번호, 페이지 크기, 정렬 정보) 사용
+		 */
+		// 페이지 조회 조건 (현재 페이지, 페이지당 글 수, 정렬 순서, 정렬 기준 컬럼)
+		Pageable pageable = PageRequest.of(
+				page, pageSize, Sort.Direction.DESC, "boardNum");
+		
+		Page<BoardEntity> entityPage = switch (searchType) {
+			// 1. 제목에 searchWord가 포함된 데이터 조회
+			case "title" -> br.findByTitleContaining(searchWord, pageable);
+			// 2. 내용에 searchWord가 포함된 데이터 조회
+			case "contents" -> br.findByContentsContaining(searchWord, pageable);
+			// 3. 작성자 ID가 searchWord가 일치하는 데이터 조회
+			case "id" -> br.findByMember_MemberId(searchWord, pageable);
+			// 4. 제목, 내용, 작성자 ID 모두 포함하는 통합 검색
+			case "all" -> br.findByTitleContainingOrContentsContainingOrMember_MemberIdContaining(searchWord, searchWord, searchWord, pageable);
+//			br.searchAll(searchWord, pageable);
+			// 5. 기본 전체 조회
+			default -> br.findAll(pageable);
+		};
+		
+		log.debug("조회된 결과: {}", entityPage.getContent());
+		
+		List<BoardDTO> dtoList = new ArrayList<>();
+		for (BoardEntity entity : entityPage.getContent()) {
+			BoardDTO dto = BoardDTO.convertToBoardDTO(entity);
+			dtoList.add(dto);
+		}
+		Page<BoardDTO> boardDtoPage = new PageImpl<>(
+				dtoList,						// DTO로 변환된 List
+				entityPage.getPageable(),		// 몇 번째 페이지인지, 몇 개씩 조회했는지 등
+				entityPage.getTotalElements()	// 조건에 맞는 전체 데이터 개수
+		);
+		
+		return boardDtoPage;
 	}
 }
